385d90722dbcef96b11e6bcc42c474ef7fb5854f
sidv23 <sidv23@gmail.com>
2022-12-06 06:24:09 -0500

    autotune

--- haram/illustrations.jl
+++ haram/illustrations.jl
@@ -1,8 +1,26 @@
 using DrWatson, Revise;
-@quickactivate "haram";
+@quickactivate
 
 begin
     using main
-    using DynamicPPL, Bijectors
+    using DynamicPPL, Bijectors, Turing
     using LinearAlgebra, Plots, Pipe
 end
+
+
+l = @layout [
+    [b{0.1h} a{0.1w}]
+    [grid(1, 1) c{0.1w}]
+]
+
+Dy = Normal(0, 1)
+Dx = MixtureModel([Normal(-2, 1.0), Normal(2, 1.0)])
+
+
+L(x) = pdf(Dx, x[1]) * pdf(Dy, x[2])
+
+p1 = plot(Normal(), fill=0, label="", axis=false, ticks=false, permute=(:y, :x))
+p2 = plot(Normal(), fill=0, label="", axis=false, ticks=false)
+p3 = contourf()
+
+plot(, layout=l)
